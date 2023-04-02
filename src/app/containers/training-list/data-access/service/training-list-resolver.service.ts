a6f9abbc7b6a2e77ccf9ece5af6c17ceedfced19
Buğra Sessevmez <sessevmezbugra@gmail.com>
2023-04-02 08:17:20 +0300

    test

--- src/app/containers/training-list/data-access/service/training-list-resolver.service.ts
+++ src/app/containers/training-list/data-access/service/training-list-resolver.service.ts
@@ -1,5 +1,6 @@
 import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
+import { KeycloakService } from 'keycloak-angular';
 import { NGXLogger } from 'ngx-logger';
 import { filter, map, of, take, withLatestFrom } from 'rxjs';
 import { TrainingListFacade } from 'src/app/containers/training-list/data-access/+state/training-list.facade';
@@ -10,6 +11,7 @@ export const trainingListResolver: ResolveFn<boolean> = () => {
   const authFacade = inject(AuthFacade);
   const trainingListFacade = inject(TrainingListFacade);
   const logger = inject(NGXLogger);
+  const keycloakService = inject(KeycloakService);
 
   return authFacade.isLoggedIn$.pipe(
     filter((isLoggedIn) => {
@@ -20,6 +22,7 @@ export const trainingListResolver: ResolveFn<boolean> = () => {
     withLatestFrom(authFacade.userProfile$),
     map(([isLoggedIn, userProfile]) => {
       logger.log("trainingListResolver", "userProfile", userProfile)
+      // logger.log("trainingListResolver", "keycloakService", keycloakService.getToken().then())
       trainingListFacade.loadTrainingsByUserId(userProfile.id!);
       return true;
     })
